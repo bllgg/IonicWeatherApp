933736c310b0fb3707eca92839f082622d37cdb9
Lahiru <gunathilakebllg@gmail.com>
2018-07-31 17:20:10 +0000

    Added contacts page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { SettingsPage } from '../settings/settings';
 import { HomePage } from '../home/home';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +11,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = SettingsPage;
+  tab2Root = SettingsPage;
+  tab3Root = AboutPage;
+  tab4Root = ContactPage;
 
   constructor() {
 
